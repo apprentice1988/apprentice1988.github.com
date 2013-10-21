9e0a638118f0d5d4d3d149d8bdd9515e31000c89
jizilong <jizilong1988@163.com>
2013-10-21 20:45:08 +0800

    up

--- js/post.js
+++ js/post.js
@@ -12,8 +12,8 @@ $(document).ready(function() {
 	$("#back-top").on("click",function(){
 		$('html, body').animate({ scrollTop: 0 }, 'fast');
 	})
-	
+
 	$("#back-top-image").on("click",function(){
-		#("html,body").animate({scrollTop: 0},'fast');
+		$("html,body").animate({scrollTop: 0},'fast');
 	})
 })
